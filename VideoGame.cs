5f27dae1c2fb50f4bc640052d0a82cc892051a05
Adam Cummins <adamrobertcummins@gmail.com>
2021-03-13 17:16:51 -0500

    Initial Commit, created VideoGame class and add method

---
+++ VideoGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class VideoGame
+{
+
+	public string Title { get; set; }
+	public int Rating { get; set; }
+	public string ESRB { get; set; }
+	
+	public void addGame()
+	{
+		List<string> Title = new List<string>();
+		List<double> Rating = new List<double>();
+		List<string> ESRB = new List<string>();
+
+		Console.WriteLine("Enter a video game title: ");
+			var NewTitle = Console.ReadLine();
+
+		Console.WriteLine("Enter a rating from 1-10 (decimals allowed): ");
+			double NewRating = Convert.ToDouble(Console.ReadLine());
+
+		Console.WriteLine("Enter the ERSB rating (E, T, M, etc.): ");
+			var NewESRB = Console.ReadLine().ToUpper();
+
+		Title.Add(NewTitle);
+		Rating.Add(NewRating);
+		ESRB.Add(NewESRB);
+
+	}
+}
