6157aad524c0fe373222d4069ce66c4ce10a2238
Adam Cummins <adamrobertcummins@gmail.com>
2021-03-15 17:52:03 -0400

    Added option to export list to Excel file

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace GameLogger
 			{
 				Console.WriteLine($"Welcome to Video Game Logger, {name}!");
 				
-				Console.Write("Please enter a number to make a selection: \n1 - Add new game \n2 - View games in list \nAny other character - Exit program \nSelection: ");
+				Console.Write("Please enter a number to make a selection: \n1 - Add new game \n2 - View games in list \n3 - Export list to Excel \n4 - Exit program \nSelection: ");
 				var choice  = (Console.ReadLine());
 				Console.WriteLine();
 
@@ -38,9 +38,21 @@ namespace GameLogger
 						Console.WriteLine();
 					}
 
-				else
+				else if(choice == "3")
+					{	
+						string CSVPath = "CSV/GameList.csv";
+						String csv = String.Join(Environment.NewLine, Title.Select(g => $"{g}"));
+						System.IO.File.WriteAllText(CSVPath, csv);
+					}
+
+				else if(choice == "4")
 					{
 						Environment.Exit(0);
+					}
+
+				else
+					{
+						Console.WriteLine("Invalid Entry");
 					}
 			}
 
